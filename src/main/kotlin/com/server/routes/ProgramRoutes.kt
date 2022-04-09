c9dd9c5ddaef75ae29309b65bbd4004ad7b0b00e
glassnekeep <56785261+glassnekeep@users.noreply.github.com>
2022-04-09 11:56:34 +0300

    Finished configuring routing. It still requires refactoring, response codes should correctly represent the http status

--- src/main/kotlin/com/server/routes/ProgramRoutes.kt
+++ src/main/kotlin/com/server/routes/ProgramRoutes.kt
@@ -1,11 +1,26 @@
 package com.server.routes
 
 import com.server.database.dao.Dao
+import com.server.models.Program
 import io.ktor.application.*
 import io.ktor.auth.*
 import io.ktor.http.*
+import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
+
+private fun Route.getAllPrograms(dao: Dao) {
+    route("/programs") {
+        get {
+            val programList = dao.getAllPrograms()
+            if (programList.isNotEmpty()) {
+                call.respond(status = HttpStatusCode.OK, programList)
+            } else {
+                call.respondText("No programs exist", status = HttpStatusCode.NotFound)
+            }
+        }
+    }
+}
 
 private fun Route.operateProgram(dao: Dao) {
     route("/program") {
@@ -27,8 +42,66 @@ private fun Route.operateProgram(dao: Dao) {
                 return@get call.respond(status = HttpStatusCode.OK, programList)
             } else {
                 return@get call.respondText(
-                    "No users connected to this program", status = HttpStatusCode.NotFound)
+                    "No programs connected to this user", status = HttpStatusCode.NotFound)
             }
+        }
+        get("/users/{id}") {
+            val id = call.parameters["id"]?.toInt() ?: return@get call.respondText(
+                "Missing or malformed id", status = HttpStatusCode.BadRequest
+            )
+            val userList = dao.getUserListWithProgramId(id)
+            if (userList.isNotEmpty()) {
+                return@get call.respond(status = HttpStatusCode.OK, userList)
+            } else {
+                return@get call.respondText(
+                    "No users connected to this program", status = HttpStatusCode.NotFound
+                )
+            }
+        }
+        post {
+            val program = call.receive<Program>()
+            program.let {
+                dao.createProgram(it.interval, it.exercise, it.users)
+            }
+            call.respondText("Program created successfully", status = HttpStatusCode.Created)
+        }
+        post("/new/{id}") {
+            val id = call.parameters["id"]?.toInt() ?: return@post call.respondText(
+                "Missing of malformed id", status = HttpStatusCode.BadRequest
+            )
+            val exercise = call.request.queryParameters["id"]?.toInt()
+            val user = call.request.queryParameters["user"]?.toInt()
+            if (exercise != null) {
+                dao.addExerciseToProgram(exercise, id)
+                return@post call.respondText(
+                    "Exercise successfully added to the program", status = HttpStatusCode.Accepted)
+            }
+            if (user != null) {
+                dao.addUserToProgram(id, user)
+                return@post call.respondText(
+                    "User successfully added to the program", status = HttpStatusCode.Accepted
+                )
+            }
+            call.respondText("Missing or malformed query parameters", status = HttpStatusCode.BadRequest)
+        }
+        put("/{id}") {
+            val id = call.parameters["id"]?.toInt() ?: return@put call.respondText(
+                "Invalid or malformed id",
+                status = HttpStatusCode.BadRequest
+            )
+            id.let {
+                val program = call.receive<Program>()
+                dao.updateProgram(id, program.interval, program.exercise, program.users)
+                return@put call.respondText("program updated successfully", status = HttpStatusCode.Accepted)
+            }
+        }
+        delete("/{id}") {
+            val id = call.parameters["id"]?.toInt() ?: return@delete call.respondText(
+                "Missing or malformed id", status = HttpStatusCode.BadRequest
+            )
+            //TODO здесь и в подобных местах нужно проверять что такой пользователь есть в базе данных и отправлять соответствующий response
+            dao.deleteProgram(id)
+            call.respondText("Program deleted successfully", status = HttpStatusCode.Accepted)
         }
     }
 }
@@ -38,5 +111,6 @@ fun Application.registerProgramRoutes(dao: Dao) {
         authenticate("auth-basic-hashed") {
             operateProgram(dao)
         }
+        getAllPrograms(dao)
     }
 }
